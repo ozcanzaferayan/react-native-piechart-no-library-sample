609588a8b2c030963d8a63322862162e7bb3cd28
Zafer AYAN <ozcanzaferayan@gmail.com>
2020-05-04 06:38:32 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,117 +1,88 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Svg, {Path, Text, G} from 'react-native-svg';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const colors = [
+  '#0d47a1',
+  '#1976D2',
+  '#2196F3',
+  '#64B5F6',
+  '#BBDEFB',
+  '#E3F2FD',
+];
 
-declare const global: {HermesInternal: null | {}};
+const getXY = (percent: number, radius: number) => {
+  const x = Math.cos((2 * Math.PI * percent) / 100) * radius;
+  const y = Math.sin((2 * Math.PI * percent) / 100) * radius;
+  return {x, y};
+};
+
+const slices = [
+  {percent: 12, id: 1},
+  {percent: 66, id: 2},
+  {percent: 5, id: 3},
+  {percent: 17, id: 4},
+].sort((a, b) => b.percent - a.percent);
 
 const App = () => {
+  const radius = 100;
+  let tempPercent = 0;
+  let tempTextPercent = 0;
+  let prevMiddlePercent = 0;
+
+  const pathDataList = slices.map((v) => {
+    const start = getXY(tempPercent, radius);
+    tempPercent += v.percent;
+    const end = getXY(tempPercent, radius);
+    const largeArcFlag = v.percent > 50 ? 1 : 0;
+    tempTextPercent += prevMiddlePercent + v.percent / 2;
+    prevMiddlePercent = v.percent / 2;
+    return {
+      d: `M ${start.x} ${start.y}
+      A ${radius} ${radius} 0 ${largeArcFlag} 1 ${end.x} ${end.y}
+      L 0 0`,
+      id: v.id,
+      textPos: {
+        x: getXY(tempTextPercent, radius / 2).x,
+        y: getXY(tempTextPercent, radius / 2).y,
+      },
+      percent: v.percent,
+    };
+  });
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={styles.safeAreaView}>
+      <Svg
+        viewBox="-100 -50 200 200"
+        style={{
+          transform: [{rotateZ: '-90deg'}],
+        }}>
+        {pathDataList.map((v, i) => (
+          <G key={v.id}>
+            <Path d={v.d} fill={colors[i]} />
+            <Text
+              x={v.textPos.x}
+              y={v.textPos.y}
+              fill="#fff"
+              fontSize="16"
+              textAnchor="middle"
+              transform={`rotate(90 ${v.textPos.x},${v.textPos.y})`}>
+              {v.percent}
+            </Text>
+          </G>
+        ))}
+      </Svg>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  safeAreaView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 2,
   },
 });
 
